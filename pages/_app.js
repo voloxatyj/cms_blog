10599e2a5c7af97ce10bed738e0d6224fbbcec14
voloxatyj <ostapmedunskij@gmail.com>
2022-01-02 19:40:33 +0200

    Initialize project. Add Layout,Header,Categories,PostCard,PostWidget components. Add some styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+import { Layout } from '../components';
+
+import '../styles/globals.scss';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp
+export default MyApp;
